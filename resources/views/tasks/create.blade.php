232c0ac70a1fe79b68299a91a4037abe6ac0be67
EC2 Default User <ec2-user@ip-172-31-35-234.us-east-2.compute.internal>
2018-07-11 02:30:16 +0000

    add_validation

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -9,6 +9,9 @@
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
 
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
+        
         {!! Form::submit('投稿') !!}
 
     {!! Form::close() !!}
