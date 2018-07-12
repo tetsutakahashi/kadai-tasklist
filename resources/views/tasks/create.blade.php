08c3e00a70da7dc1d86d446da185f47b6fe9be39
EC2 Default User <ec2-user@ip-172-31-35-234.us-east-2.compute.internal>
2018-07-12 01:25:20 +0000

    bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,7 +5,7 @@
     <h1>タスク新規作成ページ</h1>
 
     <div class="row">
-        <div class="col-xs-6">
+        <div class="col-sm-offset-2 col-sm-8" class="col-lg-offset-3 col-sm-6">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
         
                 {!! Form::label('content', 'タスク:') !!}
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,16 +4,16 @@
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
         
-        {!! Form::submit('更新') !!}
-
-    {!! Form::close() !!}
+                {!! Form::label('content', 'タスク:') !!}
+                {!! Form::text('content') !!}
+        
+                {!! Form::label('status', 'ステータス:') !!}
+                {!! Form::text('status') !!}
+                
+                {!! Form::submit('更新') !!}
+        
+            {!! Form::close() !!}
 
 @endsection
